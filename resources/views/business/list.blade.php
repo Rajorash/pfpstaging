0e3106a0a972995a80f149ede09a3959241b8a1b
Shaun Simpson <shaun@pouncemarketing.com.au>
2020-09-03 07:45:04 +0000

    Updated layout of business view.

--- resources/views/business/list.blade.php
+++ resources/views/business/list.blade.php
@@ -7,15 +7,37 @@
             <div class="card">
                 <div class="card-header"><strong>Businesses Visible To You</strong></div>
 
-                <div class="card-body">
-                @forelse ($businesses as $business)
-                    <a href="/business/{{$business->id}}"><strong>{{ $business->name }}</strong></a><br>
-                    Owner: <a href="/user/{{$business->owner->id}}">{{$business->owner->name}}</a><br>
-                    Advisor: {{$business->license ? $business->license->advisor->name : 'No advisor.'}}<br>
-                    <br>
-                @empty
-                    No Businesses!
-                @endforelse
+                <div class="card-body p-0">
+                <table class="table table-striped">
+                    <thead class="thead-inverse">
+                        <tr>
+                            <th>Business Name</th>
+                            <th>Owner</th>
+                            <th>Advisor</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        @forelse ($businesses as $business)
+                        <tr>
+                            <td scope="row">
+                                <a href="/business/{{$business->id}}"><strong>{{ $business->name }}</strong></a>
+                            </td>
+                            <td>
+                                <a href="/user/{{$business->owner->id}}">{{$business->owner->name}}</a>
+                            </td>
+                            <td>
+                                {{$business->license ? $business->license->advisor->name : 'No advisor.'}}
+                            </td>
+                        </tr>
+                        @empty
+                        <tr>
+                            <td scope="row">N/A</td>
+                            <td>N/A</td>
+                            <td>N/A</td>
+                        </tr>
+                        @endforelse
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
