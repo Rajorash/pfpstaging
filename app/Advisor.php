454e137bacd15f889c48535ba784837a99573f8f
Shaun Simpson <shaun@pouncemarketing.com.au>
2020-09-17 01:58:53 +0000

    Removed class constant as self::DEFAULT_SEATS does not improve readability

--- app/Advisor.php
+++ app/Advisor.php
@@ -6,11 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Advisor extends Model
 {
-    const DEFAULT_SEATS = 5;
-
     protected $fillable = ['niche', 'tier'];
 
-    public function __construct($user_id, $seats = DEFAULT_SEATS, $niche = null, $tier = null)
+    public function __construct($user_id, $seats = 5, $niche = null, $tier = null)
     {
         $this->id = $user_id;
         $this->seat_limit = $seats;
