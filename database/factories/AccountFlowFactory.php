cf7514116a8af996fc239fb830b2b92f9a9b242a
Shaun Simpson <shaun@pouncemarketing.com.au>
2020-10-19 22:47:21 +0000

    Cleaned up migrations and fixed account seeding

--- database/factories/AccountFlowFactory.php
+++ database/factories/AccountFlowFactory.php
@@ -7,7 +7,7 @@ use Faker\Generator as Faker;
 
 $factory->define(AccountFlow::class, function (Faker $faker) {
     return [
-        'label' => ucfirst($faker->words(mt_rand(1, 3))),
+        'label' => ucfirst($faker->words(mt_rand(1, 3), true)),
         'negative_flow' => $faker->boolean(),
         'account_id' => App\BankAccount::all()->random(),
     ];
--- database/migrations/2020_08_06_040743_create_rollout_tables.php
+++ database/migrations/2020_08_06_040743_create_rollout_tables.php
@@ -13,28 +13,35 @@ class CreateRolloutTables extends Migration
      */
     public function up()
     {
-        Schema::create('rollouts', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('business_id');
-            $table->foreign('business_id')->references('id')->on('businesses')->onDelete('cascade'); // If a business is deleted, cascade to delete their rollout
-            $table->timestamps();
-        });
         Schema::create('phases', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('rollout_id');
-            $table->foreign('rollout_id')->references('id')->on('rollouts')->onDelete('cascade'); // If an rollour is deleted, cascade to delete their phases
+            $table->unsignedBigInteger('business_id');
+            $table->foreign('business_id')
+            ->references('id')
+            ->on('businesses')
+            ->onDelete('cascade'); // If a business is deleted, cascade to delete their phases
+            $table->date('end_date')->nullable();
             $table->timestamps();
         });
+
         Schema::create('allocations', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->unsignedBigInteger('phase_id');
+            $table->foreign('phase_id')->references('id')->on('phases')->onDelete('cascade'); // If an allocation is deleted, cascade to delete their pivot table rows
+            $table->bigInteger('allocatable_id')->unsigned();
+            $table->string('allocatable_type');
+            $table->decimal('amount', 14, 2);
+            $table->date('allocation_date');
             $table->timestamps();
         });
-        Schema::create('allocation_bank_account', function (Blueprint $table) {
+
+        Schema::create('allocation_percentages', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->unsignedBigInteger('allocation_id');
-            $table->foreign('allocation_id')->references('id')->on('allocations')->onDelete('cascade'); // If an allocation is deleted, cascade to delete their pivot table rows
+            $table->unsignedBigInteger('phase_id');
+            $table->foreign('phase_id')->references('id')->on('phases')->onDelete('cascade'); // If an allocation is deleted, cascade to delete their pivot table rows
             $table->unsignedBigInteger('bank_account_id');
             $table->foreign('bank_account_id')->references('id')->on('bank_accounts')->onDelete('cascade'); // If an allocation is deleted, cascade to delete their pivot table rows
+            $table->decimal('percent', 5,2);
             $table->timestamps();
         });
     }
@@ -46,9 +53,8 @@ class CreateRolloutTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('rollouts');
         Schema::dropIfExists('phases');
         Schema::dropIfExists('allocations');
-        Schema::dropIfExists('allocation_bank_account');
+        Schema::dropIfExists('allocation_percentages');
     }
 }
