c68e7537c131ad9beef581045ae5d2c9d57f1500
Shaun <shaun@pouncemarketing.com.au>
2020-10-21 15:22:50 +1100

    Added allocate function to Allocatable trait. Affects BankAccount + AccountFlow models

--- app/Traits/Allocatable.php
+++ app/Traits/Allocatable.php
@@ -2,10 +2,30 @@
 
 namespace App\Traits;
 
+use App\Allocation as Allocation;
+use Carbon\Carbon as Carbon;
+
 trait Allocatable
 {
     public function allocations()
     {
         return $this->morphMany('App\Allocation', 'allocatable');
     }
+
+    public function allocate($amount, $phase_id = 1, $date = null)
+    {
+        // check input
+
+        $date = $date ?? Carbon::now();
+
+        $allocation = new Allocation();
+        $allocation->amount = $amount;
+        $allocation->phase_id = $phase_id;
+        $allocation->allocatable_id = $this->id;
+        $allocation->allocatable_type = get_class($this);
+        $allocation->allocation_date = $date;
+
+        $allocation->save();
+    }
+
 }
