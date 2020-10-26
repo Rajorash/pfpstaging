1bcda34e930d2ebf06add5eb534bbae70f4d0edd
Shaun <shaun@pouncemarketing.com.au>
2020-10-26 15:56:57 +1100

    Updated phase model

--- app/Phase.php
+++ app/Phase.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Phase extends Model
 {
-    //
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ['end_date'];
+    
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = ['end_date' => 'date'];
+
+    public function business() {
+        $this->belongsTo(Business::class);
+    }
+    
+    public function allocations() {
+        $this->hasMany(Allocations::class);
+    }
+
 }
