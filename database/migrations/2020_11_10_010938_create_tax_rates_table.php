985f6e227194b16591d0210925707741cb2bf412
Shaun <shaun@pouncemarketing.com.au>
2020-11-10 14:01:18 +1100

    Write migration file for tax-rates

---
+++ database/migrations/2020_11_10_010938_create_tax_rates_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTaxRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tax_rates', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('rate', 5, 2);
+            $table->foreign('bank_account_id')->references('id')->on('bank_accounts')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tax_rates');
+    }
+}
