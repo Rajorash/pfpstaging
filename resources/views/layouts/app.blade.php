e4d8f9a4895ae1095a8aaf4c80e5ed8bec8f5f49
Shaun <shaun@pouncemarketing.com.au>
2020-12-03 07:23:28 +1100

    Separate vendor and app js. Add versioning for cache busting

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,9 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    
+    <script src="{{ asset('js/vendor.js') }}" defer></script>
+    <script src="{{ asset('js/manifest.js') }}" defer></script>
+
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
