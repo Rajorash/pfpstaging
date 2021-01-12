7599730f776acb0d07a316411e5d76a5771c8d46
Shaun <shaun@pouncemarketing.com.au>
2021-01-13 10:24:14 +1100

    Disable registration - old approach pre Laravel 8/Jetstream no longer working - removed redundant line from web routes - disabled registration feature in config/fortify.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes(['register' => false]);
-
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::group(['middleware' => 'auth'], function () {
