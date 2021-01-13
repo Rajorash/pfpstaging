860a796ed0763f5e7b319349edcb4532cbc2ae15
Shaun <shaun@pouncemarketing.com.au>
2021-01-13 16:08:34 +1100

    Convert business.show view

--- resources/views/business/show.blade.php
+++ resources/views/business/show.blade.php
@@ -1,20 +1,20 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Businesses > Details
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="container mx-auto sm:px-4">
-    <div class="flex flex-wrap  justify-center">
-        <div class="md:w-2/3 pr-4 pl-4">
-            <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
-                <div class="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900"><strong>This business is:</strong></div>
+    <x-ui.card>
 
-                <div class="flex-auto p-6">
-                    <a href="/business/{{$business->id}}"><strong>{{ $business->name }}</strong></a><br>
-                    Owner: <a href="/user/{{$business->owner->id}}">{{$business->owner->name}}</a><br>
-                    Advisor: {{$business->license ? $business->license->advisor->name : 'No advisor.'}}<br>
-                    <br>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+        <x-slot name="header">
+            <h2 class="text-lg leading-6 font-medium text-black">This business is:</h2>
+        </x-slot>
+
+        <a class="text-blue-500 hover:text-blue-700" href="/business/{{$business->id}}"><strong>{{ $business->name }}</strong></a><br>
+        Owner: <a class="text-blue-500 hover:text-blue-700" href="/user/{{$business->owner->id}}">{{$business->owner->name}}</a><br>
+        Advisor: {{$business->license ? $business->license->advisor->name : 'No advisor.'}}
+
+    </x-ui.card>
+
+</x-app-layout>
