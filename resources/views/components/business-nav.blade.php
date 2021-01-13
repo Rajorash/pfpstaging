cc35d04a21390bb4f9772352b312eb1d96f45ef2
Shaun <shaun@pouncemarketing.com.au>
2021-01-13 16:48:14 +1100

    Update business nav stylings

--- resources/views/components/business-nav.blade.php
+++ resources/views/components/business-nav.blade.php
@@ -1,12 +1,12 @@
-<div class="container mx-auto sm:px-4 secondary-nav flex flex-wrap list-none pl-0 mb-0 mt-n3 mb-2">
+<div class="ml-auto sm:px-4 secondary-nav flex flex-wrap list-none pl-0 mb-0 mt-n3 mb-2">
     @foreach ($links as $link => $label)
 
     @if ($loop->first)
-    <a class="ml-auto mr-2" href="{{$link}}">{{$label}}</a> |
+    <a class="text-blue-500 hover:text-blue-600 hover:underline ml-auto mr-2" href="{{$link}}">{{$label}}</a> |
     @elseif (!$loop->last)
-    <a class="mx-2" href="{{$link}}">{{$label}}</a> |
+    <a class="text-blue-500 hover:text-blue-600 hover:underline mx-2" href="{{$link}}">{{$label}}</a> |
     @else
-    <a class="ml-2" href="{{$link}}">{{$label}}</a>
+    <a class="text-blue-500 hover:text-blue-600 hover:underline ml-2" href="{{$link}}">{{$label}}</a>
     @endif
 
     @endforeach
