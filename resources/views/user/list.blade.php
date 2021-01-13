a0e699fca22ef3fb43aa1898ec2bf8f4118ba682
Shaun <shaun@pouncemarketing.com.au>
2021-01-13 12:44:29 +1100

    Update views - fixing user list view. - modified layout to improve ui with tailwind

--- resources/views/user/list.blade.php
+++ resources/views/user/list.blade.php
@@ -1,27 +1,40 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Businesses') }}
+        </h2>
+    </x-slot>
 
-@extends('layouts.app')
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
 
-@section('content')
-<div class="container mx-auto sm:px-4">
-    <div class="flex flex-wrap  justify-center">
-        <div class="md:w-2/3 pr-4 pl-4">
-            <div class="relative flex flex-col min-w-0 rounded break-words border bg-white border-1 border-gray-300">
-                <div class="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900"><strong>Users Visible To You</strong></div>
+                <div class="py-3 px-6 mb-0 bg-gray-200 border-b-1 border-gray-300 text-gray-900 flex items-center justify-between">
+                    <h2 class="text-lg leading-6 font-medium text-black">Users Visible To You</h2>
+                    @if(Auth::user()->roles->pluck('name'))
+                    <a href="/user/create" class="group flex items-center text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-green-800 text-white hover:bg-green-900">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="w-4 h-4 mr-2"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M18 9v3m0 0v3m0-3h3m-3 0h-3m-2-5a4 4 0 11-8 0 4 4 0 018 0zM3 20a6 6 0 0112 0v1H3v-1z" /></svg>
+                        Create Client
+                    </a>
+                    @endif
+                </div>
 
                 <div class="flex-auto p-6">
-                @forelse ($users as $user)
-                    <a href="/user/{{$user->id}}"><strong>{{ $user->name }}</strong></a><br>
-                    <br>
-                @empty
+                    @forelse ($users as $user)
+                    <div class="flex items-center justify-between pb-2">
+                        <h3>{{$user->name}}</h3>
+                        <a href="/user/{{$user->id}}" class="group flex items-center text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-500 text-white hover:bg-blue-600">
+                            See Client
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="white" viewBox="0 0 24 24" stroke="currentColor" class="w-5 h-5 ml-2"><path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" /></svg>
+                        </a>
+                    </div>
+                    @empty
                     No users!
-                @endforelse
-                <br>
-                @if(Auth::user()->roles->pluck('name'))
-                <a href="/user/create" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-teal-500 text-white hover:bg-teal-600">Create Client</a>
-                @endif
+                    @endforelse
                 </div>
+
             </div>
         </div>
     </div>
 </div>
-@endsection
+</x-layout-app>
