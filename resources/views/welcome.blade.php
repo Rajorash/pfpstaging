0957904bcd4ced47e89342f00932f87be99a5c7d
Shaun <shaun@pouncemarketing.com.au>
2021-01-14 12:44:41 +1100

    Fix spacing of splash page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,20 +1,24 @@
 <x-guest-layout>
+    <div class="flex flex-col h-screen w-screen">
     @if (Route::has('login'))
-    <div class="flex flex-row content-end w-full space-x-6 py-3 px-6">
-        @auth
-        <a class="text-blue-400 hover:text-blue-600 block ml-auto" href="{{ url('/dashboard') }}">Dashboard</a>
-        @else
-        <a class="text-blue-400 hover:text-blue-600 block ml-auto" href="{{ route('login') }}">Login</a>
+    <div>
+        <div class="flex flex-row content-end w-full space-x-6 py-3 px-6">
+            @auth
+            <a class="text-blue-400 hover:text-blue-600 block ml-auto" href="{{ url('/dashboard') }}">Dashboard</a>
+            @else
+            <a class="text-blue-400 hover:text-blue-600 block ml-auto" href="{{ route('login') }}">Login</a>
 
-        @if (Route::has('register'))
-        <a class="text-blue-400 hover:text-blue-600 block" href="{{ route('register') }}">Register</a>
-        @endif
-        @endauth
+            @if (Route::has('register'))
+            <a class="text-blue-400 hover:text-blue-600 block" href="{{ route('register') }}">Register</a>
+            @endif
+            @endauth
+        </div>
     </div>
     @endif
 
-    <div class="container mx-auto w-full">
+    <div class="flex-grow">
         <h1 class="font-sans text-gray-600 subpixel-antialiased text-center text-5xl">PFP MVP</h1>
         <h2 class="font-sans text-gray-600 subpixel-antialiased text-center text-xl">Coming soon</h2>
+    </div>
     </div>
 </x-guest-layout>
