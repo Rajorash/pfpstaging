2f66d796c324a5d5280ee3fede0c0e68c2b90490
Shaun <shaun@pouncemarketing.com.au>
2021-02-04 12:41:03 +1100

    Fix radio select bug from framework change on create flow

--- resources/views/accounts/createflow.blade.php
+++ resources/views/accounts/createflow.blade.php
@@ -37,12 +37,12 @@
                 <div class="mb-4 flex flex-wrap ">
                     <label class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0 leading-normal md:text-right">Flow Type:</label>
                     <div class="md:w-1/2 pr-4 pl-4">
-                        <div class="relative inline-flex align-middle" data-toggle="buttons">
-                            <label class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-green-500 text-white hover:green-600 active">
+                        <div class="relative inline-flex align-middle" >
+                            <label class="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-2 pr-3 leading-normal no-underline">
                                 <input type="radio" name="flow-direction" id="flow-in" autocomplete="off" value="0" checked>
                                 Positive
                             </label>
-                            <label class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-red-600 text-white hover:bg-red-700">
+                            <label class="inline-block align-middle text-center select-none font-normal whitespace-no-wrap py-2 pr-3 leading-normal no-underline">
                                 <input type="radio" name="flow-direction" id="flow-out" autocomplete="off" value="1">
                                 Negative
                             </label>
@@ -57,6 +57,7 @@
                 </div>
 
                 <div class="mb-4 flex flex-wrap  mb-0">
+                    <div class="md:w-1/3 pr-4 pl-4 pt-2 pb-2 mb-0"></div>
                     <div class="md:w-1/2 pr-4 pl-4 md:mx-1/3">
                         <button type="submit" class="inline-block align-middle text-center select-none border font-normal whitespace-no-wrap rounded py-1 px-3 leading-normal no-underline bg-blue-600 text-white hover:bg-blue-600">
                             {{ __('Add Flow') }}
