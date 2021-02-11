02892bcf47cb64e8ce15848d9fc957becbf39a95
Shaun <shaun@pouncemarketing.com.au>
2021-02-11 15:16:37 +1100

    Update Allocatable trait functions - allocate() now uses updateOrCreate() - added getAllocationByDate($date) - added getAllocationAmount($date)

--- app/Traits/Allocatable.php
+++ app/Traits/Allocatable.php
@@ -7,18 +7,31 @@ use Carbon\Carbon as Carbon;
 
 trait Allocatable
 {
+    /**
+     * Returns a collection of all associated allocations.
+     *
+     * @return Collection
+     */
     public function allocations()
     {
         return $this->morphMany('App\Models\Allocation', 'allocatable');
     }
 
+    /**
+     * Make an allocation against the Aloocatable object. Amount is
+     *  required. default $date is current date and default phase is 1
+     *
+     * @param double $amount
+     * @param date $date
+     * @param integer $phase_id
+     * @return Allocation
+     */
     public function allocate($amount, $date = null, $phase_id = 1)
     {
         // check input
 
         $date = $date ?? Carbon::now();
 
-        // TODO: change this to updateOrCreate
         $allocation = Allocation::updateOrCreate([
             'allocatable_id' => $this->id,
             'allocatable_type' => get_class($this),
@@ -30,6 +43,36 @@ trait Allocatable
         ]);
 
         return $allocation;
+    }
+
+    /**
+     * Return an allocation for the current Allocatable model for the given
+     * date if one exists
+     *
+     * @param date|string $date
+     * @return void
+     */
+    public function getAllocationByDate($date) {
+        return Allocation::where(
+            ['allocatable_type' => get_class($this),
+            'allocatable_id' => $this->id,
+            'allocation_date' => $date]
+        )->firstOrFail();
+    }
+
+    /**
+     * Return an allocation for the current Allocatable model for the given
+     * date if one exists
+     *
+     * @param date|string $date
+     * @return void
+     */
+    public function getAllocationAmount($date) {
+        return Allocation::where(
+            ['allocatable_type' => get_class($this),
+            'allocatable_id' => $this->id,
+            'allocation_date' => $date]
+        )->first()->amount;
     }
 
 }
