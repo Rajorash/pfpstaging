e2bde50c9d1a0c5827bfc46765faa5bf0b9f37e5
Shaun <shaun@pouncemarketing.com.au>
2021-03-09 16:02:49 +1100

    Add zxcvbn min score of 3 requirement to password validator

--- app/Actions/Fortify/PasswordValidationRules.php
+++ app/Actions/Fortify/PasswordValidationRules.php
@@ -3,6 +3,7 @@
 namespace App\Actions\Fortify;
 
 use Laravel\Fortify\Rules\Password;
+use Olssonm\Zxcvbn\Facades\Zxcvbn;
 
 trait PasswordValidationRules
 {
@@ -13,6 +14,6 @@ trait PasswordValidationRules
      */
     protected function passwordRules()
     {
-        return ['required', 'string', new Password, 'confirmed'];
+        return ['required', 'string', new Password, 'confirmed', 'zxcvbn_min:3' ];
     }
 }
