fd111ab0bb321cf9478935a1233004974d90961a
Shaun <shaun@pouncemarketing.com.au>
2021-03-26 12:34:29 +1100

    Remove debugbar disable code causing issues on non dev env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Business::observe(BusinessObserver::class);
-        \Debugbar::disable();
+        // \Debugbar::disable();
     }
 }
