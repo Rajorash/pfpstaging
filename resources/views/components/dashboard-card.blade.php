54f6ade69a939097ce61e8325bf9bdd50980fdf5
mhladchenko <myhailo@hotmail.com>
2021-04-02 11:58:52 +0300

    dashboard layout

---
+++ resources/views/components/dashboard-card.blade.php
@@ -0,0 +1,21 @@
+@props(['route', 'title','linkTitle'])
+<div class="mb-2">
+    {{$icon}}
+</div>
+
+<div class="text-lg text-dark_gray2 leading-7">
+    <a href="{{$route}}">{{$title}}</a>
+</div>
+
+<div class="mt-2 text-sm">
+    {{$slot}}
+</div>
+
+<a href="{{ $route}}">
+    <div class="mt-3 flex items-center text-sm font-normal text-blue">
+        <div>{{$linkTitle}}</div>
+        <div class="ml-2 text-blue">
+            <x-icons.arrow-right/>
+        </div>
+    </div>
+</a>
