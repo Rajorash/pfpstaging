ce846b0d94073ae95d47f68f729c299ce6c064fe
mhladchenko <myhailo@hotmail.com>
2021-04-09 17:49:01 +0300

    - Refactored profile

--- resources/views/vendor/jetstream/components/button.blade.php
+++ resources/views/vendor/jetstream/components/button.blade.php
@@ -1,3 +1,4 @@
-<button {{ $attributes->merge(['type' => 'submit', 'class' => 'items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray disabled:opacity-25 transition ease-in-out duration-150']) }}>
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'text-center select-none border font-normal whitespace-no-wrap
+           rounded-lg py-2 px-6 leading-normal no-underline bg-blue text-white hover:bg-dark_gray2']) }}>
     {{ $slot }}
 </button>
