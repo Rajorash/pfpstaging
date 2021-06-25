53205525a6bb73c974930112420249b37746b16f
Maryan Bokalo <marian@pouncemarketing.com.au>
2021-06-25 17:15:59 +0300

    Add licenses increase-decrease component

---
+++ app/Http/Livewire/LicensesCounter.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class LicensesCounter extends Component
+{
+    public $initial;
+    public $user;
+
+    public function mount()
+    {
+        $this->initial = 5;
+    }
+    public function render()
+    {
+        return view('livewire.licenses-counter');
+    }
+
+    public function licincrement()
+    {
+        $this->initial+=1;
+    }
+
+    public function licdecrement()
+    {
+        if($this->initial>0){
+            $this->initial-=1;
+        }else{
+            session()->flash('info', "You cannot have negative value in the counter");
+        }
+    }
+}
