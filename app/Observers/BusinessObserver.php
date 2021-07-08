2f2ad8581c782bf1e7c9f9d1723c4161b0af5342
Shaun <shaun@pouncemarketing.com.au>
2021-07-08 16:43:23 +1000

    Update BusinessObserver to initialise default accounts on creation. - deactivate AccountSeeder call from database seeder.

--- app/Observers/BusinessObserver.php
+++ app/Observers/BusinessObserver.php
@@ -2,6 +2,8 @@
 
 namespace App\Observers;
 
+use App\Models\AccountFlow;
+use App\Models\BankAccount;
 use App\Models\Business;
 use App\Models\Phase;
 use Carbon\Carbon as Carbon;
@@ -18,7 +20,7 @@ class BusinessObserver
     {
 
         $this->initialisePhases($business);
-
+        $this->initialiseAccounts($business);
         // clear the cached all businesses object
         Cache::forget('Business_all');
 
@@ -43,5 +45,150 @@ class BusinessObserver
 
     }
 
+    /**
+     * On business creation, initialise default account setup.
+     *
+     * @param [type] $business
+     * @return void
+     */
+    private function initialiseAccounts($business)
+    {
+        $accounts = $this->getDefaultAccounts();
+
+        foreach($accounts as $acc) {
+            $account = factory(BankAccount::class)->create([
+                'name' => $acc['name'],
+                'type' => $acc['type'],
+                'business_id' => $business->id
+            ]);
+            $business->accounts()->save($account);
+
+            foreach($acc['flows'] as $flow) {
+                $new_flow = factory(AccountFlow::class)->create(['label' => $flow['label'], 'negative_flow' => $flow['negative']]);
+                $account->flows()->save($new_flow);
+            }
+        }
+    }
+
+    private function getDefaultAccounts() {
+        return  array(
+            [
+                'name' => 'Core',
+                'type' => 'revenue',
+                'flows' => [
+                    [ 'label' => "Accounts Receivable", 'negative' => false ],
+                    [ 'label' => "Estimated Activity", 'negative' => false ],
+                ]
+            ],
+            [
+                'name' => 'Drip Account',
+                'type' => 'pretotal',
+                'flows' => [
+                    [ 'label' => "Transfer to revenue", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Materials',
+                'type' => 'prereal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Purchases", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Subcontractors',
+                'type' => 'prereal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Payments", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Profit',
+                'type' => 'postreal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Distributions", 'negative' => true ],
+                    [ 'label' => "Debt pay down", 'negative' => true ],
+                ]
+
+            ],
+            [
+                'name' => 'Owners Pay',
+                'type' => 'postreal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Wages", 'negative' => true ],
+                    [ 'label' => "Downturn", 'negative' => true ],
+                ]
+
+            ],
+            [
+                'name' => 'Staff Related',
+                'type' => 'postreal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Payroll", 'negative' => true ],
+                    [ 'label' => "Downturn", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Opex',
+                'type' => 'postreal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "Rent", 'negative' => true ],
+                    [ 'label' => "Education & Training", 'negative' => true ],
+                    [ 'label' => "Promotions", 'negative' => true ],
+                    [ 'label' => "Software Expenses", 'negative' => true ],
+                    [ 'label' => "Website", 'negative' => true ],
+                    [ 'label' => "Consulting/Accounting", 'negative' => true ],
+                    [ 'label' => "Interest Expenses", 'negative' => true ],
+                    [ 'label' => "Bank Fees", 'negative' => true ],
+                    [ 'label' => "Insurance", 'negative' => true ],
+                    [ 'label' => "General Monthly Costs", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Tax',
+                'type' => 'postreal',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "BAS - Current", 'negative' => true ],
+                    [ 'label' => "Super Payments", 'negative' => true ],
+                    [ 'label' => "Payment Plans", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'G.S.T.',
+                'type' => 'salestax',
+                'flows' => [
+                    // [ 'label' => "Transfer in", 'negative' => false ],
+                    [ 'label' => "BAS - Payment", 'negative' => true ],
+                ]
+            ],
+            [
+                'name' => 'Vault',
+                'type' => 'postreal',
+                'flows' => [
+                    [ 'label' => "Transfer in", 'negative' => false ],
+                ]
+            ],
+            [
+                'name' => 'Other',
+                'type' => 'postreal',
+                'flows' => [
+                    [ 'label' => "Transfer in", 'negative' => false ],
+                ]
+            ],
+            [
+                'name' => 'Charity',
+                'type' => 'postreal',
+                'flows' => [
+                    [ 'label' => "Transfer in", 'negative' => false ],
+                ]
+            ],
+        );
+    }
 }
 
