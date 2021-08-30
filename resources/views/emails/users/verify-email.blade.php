bcbe753991042363744de2d5c33060360ffae685
shaunpounce <shaun@pouncemarketing.com.au>
2021-08-30 15:02:03 +1000

    Remove indentation from markdown mail templates to fix layout issues.

--- resources/views/emails/users/verify-email.blade.php
+++ resources/views/emails/users/verify-email.blade.php
@@ -1,28 +1,28 @@
 @component('mail::message')
-    # Hello!
+# Hello!
 
-    {{$author->name}} added you to PFP.
+{{$author->name}} added you to PFP.
 
-    Your email: {{$user->email}}
-    <br/>Your password: {{$generatedPassword}}
+Your email: {{$user->email}}
+<br/>Your password: {{$generatedPassword}}
 
-    <span style="color: red;"><b>Important! After login - change your password!</b></span>
+<span style="color: red;"><b>Important! After login - change your password!</b></span>
 
-    @component('mail::button', ['url' => $verifyUrl])
-        Verify Email Address
-    @endcomponent
+@component('mail::button', ['url' => $verifyUrl])
+Verify Email Address
+@endcomponent
 
-    If you did not create an account, no further action is required.
+If you did not create an account, no further action is required.
 
-    Thanks,<br>
-    {{ config('app.name') }}
+Thanks,<br>
+{{ config('app.name') }}
 
 
-    @slot('subcopy')
-        @lang(
-            "If you're having trouble clicking the \"Verify Email Address\" button, copy and paste the URL below\n".
-            'into your web browser:'
-        ) <span class="break-all">[{{ $verifyUrl }}]({{ $verifyUrl }})</span>
-    @endslot
+@slot('subcopy')
+@lang(
+"If you're having trouble clicking the \"Verify Email Address\" button, copy and paste the URL below\n".
+'into your web browser:'
+) <span class="break-all">[{{ $verifyUrl }}]({{ $verifyUrl }})</span>
+@endslot
 
 @endcomponent
