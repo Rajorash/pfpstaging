461c2b5f4027b7cc3865018951a98e5491a69156
mhladchenko <Deleting Bank Accounts>
2021-08-30 16:35:12 +0300

    - added a few UI elements and rearrange some classes

--- resources/views/components/ui/data-submit-controls.blade.php
+++ resources/views/components/ui/data-submit-controls.blade.php
@@ -3,12 +3,13 @@
 'heightController' => false,
 'forecastController' => false
 ])
-<div class="{{$width}} mx-auto p-2 text-sm flex justify-end items-center">
+<div class="{{$width}} mx-auto py-2 text-sm flex justify-end items-center">
 
     @if($forecastController)
-        <div title="Enabling this allows a user to manually add forecast values by double clicking them." class="flex items-center mr-4 flex-nowrap">
-            <input type="checkbox" id="allow_forecast_double_click" class="px-2 py-2 mx-3 my-3 rounded form-input"/>
-            <label for="allow_forecast_double_click">{{__('Enable manual quick add')}} <strong>[?]</strong></label>
+        <div title="Enabling this allows a user to manually add forecast values by double clicking them."
+             class="flex items-center mr-4 flex-nowrap">
+            <input type="checkbox" id="allow_forecast_double_click" class="pr-2 py-2 mx-3 my-3 rounded form-input"/>
+            <label for="allow_forecast_double_click">{{__('Enable manual quick add')}} <x-icons.question /></label>
         </div>
     @endif
 
@@ -16,16 +17,16 @@
         <div class="flex mr-4 flex-nowrap">
             <input type="radio" value="full" id="height_full" name="block_different_height"
                    class="hidden radio_as_switcher"/>
-            <label for="height_full" class="">Full height</label>
+            <label for="height_full" class="text-right whitespace-nowrap h-9">Full height</label>
             <input type="radio" value="window" id="height_window" name="block_different_height"
                    class="hidden radio_as_switcher"/>
-            <label for="height_window" class="">Height by window</label>
+            <label for="height_window" class="whitespace-nowrap h-9">Height by window</label>
         </div>
     @endif
 
     <div class="flex items-center" title="The amount of seconds that will pass before the form will automatically recalculate after updating values">
-        <input type="number" min="1" max="30" id="delay_submit_data" class="w-16 px-2 py-0 py-1 my-0 ml-3 mr-0 text-center rounded form-input">
-        <label class="ml-3" for="delay_submit_data">Auto submit delay (sec) <strong>[?]</strong></label>
+        <input type="number" min="1" max="30" id="delay_submit_data" class="w-16 h-9 my-0 ml-3 mr-0 text-center rounded form-input text-right">
+        <label class="ml-3" for="delay_submit_data">Auto submit delay (sec) <x-icons.question /></label>
     </div>
 
 </div>
