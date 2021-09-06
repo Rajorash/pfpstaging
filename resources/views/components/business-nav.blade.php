50e5ed6b3babe62788344a2c1f45bce7720e3623
mhladchenko <Deleting Bank Accounts>
2021-09-06 16:49:44 +0300

    compact elements on business-nav.blade.php

--- resources/views/components/business-nav.blade.php
+++ resources/views/components/business-nav.blade.php
@@ -11,7 +11,7 @@
             $active = request()->is('*business/*/maintenance');
         @endphp
         <a href="{{route('maintenance.business', ['business' => $business])}}" title="{{__('Maintenance')}}"
-           class="bg-white block rounded box-border p-3 flex mr-6 h-12
+           class="bg-white block rounded box-border p-3 flex mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
                ">
             <x-icons.gear :class="'h-4 w-auto my-0.5 inline-block'"/>
@@ -23,7 +23,7 @@
         $active = request()->routeIs('balance.business');
     @endphp
     <a href="{{url('/business/'.$businessId.'/balance')}}" title="{{__('Manually change balances')}}"
-       class="bg-white block rounded box-border p-3 flex mr-6 h-12
+       class="bg-white block rounded box-border p-3 flex mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
            ">
         <x-icons.balance :class="'h-6 w-auto inline-block'"/>
@@ -34,7 +34,7 @@
         $active = request()->is('*business/*/accounts');
     @endphp
     <a href="{{url('/business/'.$businessId.'/accounts')}}" title="{{__('Accounts')}}"
-       class="bg-white block rounded box-border p-3 flex mr-6 h-12
+       class="bg-white block rounded box-border p-3 flex mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
            ">
         <x-icons.vallet :class="'h-6 w-auto inline-block'"/>
@@ -45,7 +45,7 @@
         $active = request()->routeIs('allocations-percentages');
     @endphp
     <a href="{{route('allocations-percentages', ['business' => $business])}}" title="{{__('Rollout Percentages')}}"
-       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-6 h-12
+       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
            ">
         <x-icons.percent :class="'h-5 w-auto mt-1 inline-block'"/>
@@ -56,7 +56,7 @@
         $active = request()->routeIs('allocation-calculator-with-id');
     @endphp
     <a href="{{route('allocation-calculator-with-id', ['business' => $business])}}" title="{{__('Allocation Calculator')}}"
-       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-6 h-12
+       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
            ">
         <x-icons.calculator :class="'h-6 w-auto inline-block'"/>
@@ -67,7 +67,7 @@
         $active = request()->routeIs('allocations-calendar');
     @endphp
     <a href="{{route('allocations-calendar', ['business' => $business])}}" title="{{__('Projection Data Entry')}}"
-       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-6 h-12
+       class="bg-white block rounded box-border p-3 flex text-gray-700 mr-4 h-12
         @if($active) text-blue @else text-gray-700 @endif
            ">
         <x-icons.table :class="'h-6 w-auto inline-block'"/>
