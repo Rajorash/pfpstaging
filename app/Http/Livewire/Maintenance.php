e24544009c6c6e386a4e7a6be6b031fa7beb542a
mhladchenko <Deleting Bank Accounts>
2021-09-08 09:23:31 +0300

    update composer post-commands and Maintenance page

--- app/Http/Livewire/Maintenance.php
+++ app/Http/Livewire/Maintenance.php
@@ -20,14 +20,24 @@ class Maintenance extends Component
     {
         //add you command here
         $this->artisanCommands = [
-            'config:cache' => [
-                'title' => 'Create a cache file for faster configuration loading',
+            'config:clear' => [
+                'title' => 'Clear Configuration cache',
                 'command' => 'config:cache'
             ],
-            'cache:clear' => [
-                'title' => 'Flush the application cache',
-                'command' => 'cache:clear'
+            'config:cache' => [
+                'title' => 'Cache Configs',
+                'command' => 'config:cache'
             ],
+
+            'route:clear' => [
+                'title' => 'Clearing Route Cache',
+                'command' => 'route:clear'
+            ],
+            'route:cache' => [
+                'title' => 'Cache Routes',
+                'command' => 'route:cache'
+            ],
+
             'view:clear' => [
                 'title' => 'Clear all compiled view files',
                 'command' => 'view:clear'
@@ -36,14 +46,31 @@ class Maintenance extends Component
                 'title' => 'Compile all of the application\'s Blade templates',
                 'command' => 'view:cache'
             ],
+
+            'event:clear' => [
+                'title' => 'Clear Events Cache',
+                'command' => 'event:clear'
+            ],
+            'event:cache' => [
+                'title' => 'Cache Events',
+                'command' => 'event:cache'
+            ],
+
+            'cache:clear' => [
+                'title' => 'Clearing Application Cache',
+                'command' => 'cache:clear'
+            ],
+
             'optimize:clear' => [
-                'title' => 'Remove the cached bootstrap files',
+                'title' => 'Clearing All Cache',
                 'command' => 'optimize:clear'
             ],
+
             'migrate' => [
                 'title' => 'Run the database migrations',
                 'command' => 'migrate'
             ],
+
             'redis:clearall' => [
                 'title' => 'Clear Redis Cache',
                 'php_command' => '\Illuminate\Support\Facades\Cache::flush();'
